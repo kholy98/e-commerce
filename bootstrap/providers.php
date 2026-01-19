502bfc1be946c4d1df6f3089bc54211e0bc7f569
unknown <ahmadkholy98@gmail.com>
2026-01-19 11:51:03 +0200

    fix authentication

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Providers\FortifyServiceProvider;
+use Laravel\Sanctum\SanctumServiceProvider;
 
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\PaymentServiceProvider::class,
     FortifyServiceProvider::class,
+    SanctumServiceProvider::class,
 ];
