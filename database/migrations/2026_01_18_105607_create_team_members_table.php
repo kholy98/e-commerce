d78aa301e13539529a66afce6454c30477ff46a8
zoasr <44146518+ZOASR@users.noreply.github.com>
2026-01-27 17:04:48 +0200

    feat(db): create team_members table migration
    Add the id column and standard structure for team_members table

--- database/migrations/2026_01_18_105607_create_team_members_table.php
+++ database/migrations/2026_01_18_105607_create_team_members_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('team_members', function (Blueprint $table) {
+            $table->id();
             $table->string('fullname');
             $table->string('phone')->nullable();
             $table->string('email');
